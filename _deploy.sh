b4b997080af1c712166dd0d7112fc515f083f3a5
Nakorn Sinpadung <nakorn.s@cdg.co.th>
2019-08-09 14:56:27 +0700

    Fix structure for deploy

--- _deploy.sh
+++ _deploy.sh
@@ -4,12 +4,11 @@
 set -e
 
 # build
-cd demo
 npm i
 npm run build
 
 # navigate into the build output directory
-cd .vuepress/dist
+cd demo/.vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -22,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:nakorndev/vuepress-theme-bulma.git master:gh-pages
+git push -f https://github.com/nakorndev/vuepress-theme-bulma.git master:gh-pages
 
 cd -
